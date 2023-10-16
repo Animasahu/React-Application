0ebd8c6305ec151f690a8f53b464f79484e08144
Sahu <anima.sahu@capgemini.com>
2023-10-16 11:40:27 +0530

    create parcel , React.createElement ep-2

--- App.js
+++ App.js
@@ -15,13 +15,15 @@
  * 
  * 
  */
+import React from "react";
+import ReactDOM from "react-dom";
 
 const parent = React.createElement(
     "div", 
     {id: "parent"}, [
         React.createElement("div", {id: "child"}, [
-            React.createElement("h1", {}, "I'm h1 tag"),
-            React.createElement("h2", {}, "I'm h2 tag")
+            React.createElement("h1", {}, "I'm Anima Sahu"),
+            React.createElement("h2", {}, "hgjdgkwfhkwef")
     ]),
     React.createElement("div", {id: "child2"}, [
         React.createElement("h1", {}, "I'm h1 tag"),
---
+++ README.md
@@ -0,0 +1,21 @@
+#React application
+
+
+#parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replecement
+-File Watching Algorithm - Written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-Differential Bundling - support older brower
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and Production bundle
