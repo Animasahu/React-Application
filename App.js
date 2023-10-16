cae5d76fc40b04b1734c7ac29e0112661094abba
Sahu <anima.sahu@capgemini.com>
2023-10-16 15:12:40 +0530

    jsx

--- App.js
+++ App.js
@@ -1,39 +1,23 @@
-/**
- * 
- * <div id = "parent">
- *  <div id = "child">
- *    <h1>I'm h1 tag</h1>
- *    <h2>I'm h2 tag</h2>
- *  </div>
- *   <div id = "child">
- *    <h1>I'm h1 tag</h1>
- *    <h2>I'm h2 tag</h2>
- *  </div>
- * </div>
- * 
- * ReactElement(object) = HTML(Browser understand)
- * 
- * 
- */
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"}, [
-        React.createElement("div", {id: "child"}, [
-            React.createElement("h1", {}, "I'm Anima Sahu"),
-            React.createElement("h2", {}, "hgjdgkwfhkwef")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag")
-])
-]);
+//React.createElement => ReactElement - JS object =>HTMLElement(render)
+const heading = React.createElement("h1" , {id:"heading"}, "this is h1 tag");
+
+console.log(heading);
+
+//JSX - HTML-like or XML-like syntax
+//JSX (transpiled before it reaches the JS) - PARCEL - Babel
+//JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+const jsxHeading = (
+  <h1 id="heading" className="head" tabIndex="4">
+  Anima Sahu
+  </h1>
+  );
+
+console.log(jsxHeading);
 
 
-console.log(parent); //object
-        
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
